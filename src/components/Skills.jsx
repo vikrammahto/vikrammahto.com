45e687347b285e9429324b6bd7ecac24f7417b32
vikrammahto <vikramkr.in@gmail.com>
2023-08-09 11:43:20 +0530

    Minor fix

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ const Skills = () => {
   const skills = [
     {
       category: 'Languages',
-      items: ['HTML', 'CSS', 'JavaScript', 'PHP (WordPress)'],
+      items: ['HTML', 'CSS', 'JavaScript'],
     },
     {
       category: 'Frameworks',
@@ -19,18 +19,18 @@ const Skills = () => {
       items: ['Figma', 'Penpot', 'Adobe XD', 'Canva', 'Adobe Photoshop'],
     },
     {
-      category: 'Tools & Other',
-      items: ['Git', 'Github', 'Bitbucket', 'Visual Studio Code'],
+      category: 'Others',
+      items: ['WordPress', 'Git', 'Github', 'Bitbucket'],
     },
   ];
   return (
-    <div className="mt-20 flex flex-col items-start space-y-5 md:mt-32 md:space-y-7 md:px-5 lg:mt-32">
+    <div className="flex flex-col items-start mt-20 space-y-5 md:mt-32 md:space-y-7 md:px-5 lg:mt-32">
       <h1 className="text-3xl font-bold">Skills</h1>
       <div className="md:grid md:grid-cols-3 md:gap-4">
         {skills.map((skill, index) => (
           <div
             key={index}
-            className="py-3 border-dashed border-b-2 border-zinc-300 md:border-0"
+            className="py-3 border-b-2 border-dashed border-zinc-300 md:border-0"
           >
             <h2 className="text-2xl">{skill.category}</h2>
             <div className="flex">
@@ -38,7 +38,7 @@ const Skills = () => {
                 {skill.items.map((item, index) => (
                   <li
                     key={index}
-                    className="inline-block relative px-3 before:absolute before:top-1/2 before:left-0 before:-translate-y-1/2 before:w-1 before:h-1 before:bg-gray-300 before:rounded-full"
+                    className="relative inline-block px-3 before:absolute before:top-1/2 before:left-0 before:-translate-y-1/2 before:w-1 before:h-1 before:bg-gray-300 before:rounded-full"
                   >
                     {item}
                   </li>
