a2636a17daca79a991ac1cbd5fa765441d924e22
vikrammahto <vikramkr.in@gmail.com>
2025-05-01 02:20:06 +0530

    fix: button event issue due to absolute grid background

--- src/app/layout.js
+++ src/app/layout.js
@@ -64,7 +64,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${figtree.className} bg-neutral-950 text-white antialiased`}
       >
-        <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] bg-[size:24px_24px]"></div>
+        <div className="absolute pointer-events-none h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] bg-[size:24px_24px]"></div>
         <Header />
         <main>{children}</main>
         <Footer />
